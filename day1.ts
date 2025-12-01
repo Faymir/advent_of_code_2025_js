9ddbc2381c2422bd0599d9234391d777771d8f56
Faymir <faymir@guidap.co>
2025-12-01 12:14:02 +0100

    feat: refactor rotation logic to include part argument handling

--- day1.ts
+++ day1.ts
@@ -1,9 +1,19 @@
 import fs from 'node:fs/promises';
 
-function applyRotation(dial: number, r: string, password: number) {
-  let value = dial;
-  const operationSign = r[0] === 'L' ? -1 : 1;
-  value = value + (operationSign * parseInt(r.slice(1)))
+function calculateUpdatedPassword(password: number, value: number, dial: number) {
+  let counter = 0
+  if (value < 0) {
+    value = ((value % 100) + 100) % 100
+  } else if (value > 99) {
+    value = value % 100
+  }
+  if (value === 0) {
+    counter += 1
+  }
+  return { dial: value, password: password + counter };
+}
+
+function calculateUpdatedPassword2(password: number, value: number, dial: number) {
   let counter = 0
   if (value === 0) {
     counter += 1
@@ -23,23 +33,34 @@ function applyRotation(dial: number, r: string, password: number) {
     counter += Math.floor(value / 100)
     value = value % 100
   }
-	
   return { dial: value, password: password + counter };
 }
 
-try {
-  const rotations = await fs.readFile('./inputs/day1.txt', { encoding: 'utf8' });
-  let dialValue = 50;
-
-  let counter = rotations.split('\n').reduce((acc, curr) => {
-    
-  const { dial, password } = applyRotation(dialValue, curr, acc)
-    dialValue = dial
-    return password
-  }, 0)
-
-  console.log(counter, dialValue);
-} catch (err) {
-  console.error(err);
+function applyRotation(dial: number, r: string, password: number, part1 = true) {
+  let value = dial;
+  const operationSign = r[0] === 'L' ? -1 : 1;
+  value = value + (operationSign * parseInt(r.slice(1)))
+  
+	
+  return part1 
+    ? calculateUpdatedPassword(password, value, dial)
+    : calculateUpdatedPassword2(password, value, dial);
 }
+export async function run(part1: boolean = true) {
+  console.log(`Running part ${part1 ? '1' : '2'}...`);
+  try {
+    const rotations = await fs.readFile('./inputs/day1.txt', { encoding: 'utf8' });
+    let dialValue = 50;
 
+    let counter = rotations.split('\n').reduce((acc, curr) => {
+      
+    const { dial, password } = applyRotation(dialValue, curr, acc, part1)
+      dialValue = dial
+      return password
+    }, 0)
+
+    console.log(counter, dialValue);
+  } catch (err) {
+    console.error(err);
+  }
+}
